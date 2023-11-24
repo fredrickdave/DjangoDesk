6071de0f744b9605644689254cf9f38cd602ad56
Fredrick Dave Santulan <(none)>
2023-11-24 15:33:59 +0800

    Add redirect url after login

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,11 +1,16 @@
 from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import redirect, render
+from django.shortcuts import HttpResponse, redirect, render
+from django.utils.encoding import iri_to_uri
+from django.utils.http import url_has_allowed_host_and_scheme
 
 from .forms import LoginForm
 
 
-# Create your views here.
 def sign_in(request):
+    # Redirect to dashboard if user is already logged-in
+    if request.user.is_authenticated:
+        return redirect("dashboard")
+
     if request.method == "POST":
         login_form = LoginForm(request.POST)
         if login_form.is_valid():
@@ -15,7 +20,14 @@ def sign_in(request):
             user = authenticate(username=username, password=password)
             if user:
                 login(request=request, user=user)
-                return redirect(to="dashboard")
+                next_url = request.POST.get("next")
+                print("NEXT URL:", next_url)
+                # Check if "?next=" param is safe (not off-site).
+                if next_url and url_has_allowed_host_and_scheme(next_url, None):
+                    next_url = iri_to_uri(next_url)
+                    print("Nexted")
+                    return redirect(to=next_url)
+                return redirect("dashboard")
     else:
         login_form = LoginForm()
 
