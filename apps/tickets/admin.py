96a5a5c3a6462bd45b0bdef372f33b079f5af115
Fredrick Dave Santulan <(none)>
2023-12-26 14:28:46 +0800

    Register ticketattachment to admin panel

--- apps/tickets/admin.py
+++ apps/tickets/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
 
-from .models import Ticket, TicketComment, TicketPriority, TicketStatus, TicketType
+from .models import Ticket, TicketAttachment, TicketComment, TicketPriority, TicketStatus, TicketType
+
+
+class TicketAttachmentInline(admin.TabularInline):
+    model = TicketAttachment
+
+
+class TicketAdmin(admin.ModelAdmin):
+    inlines = [
+        TicketAttachmentInline,
+    ]
+
 
 # Register your models here.
-admin.site.register(Ticket)
+admin.site.register(Ticket, TicketAdmin)
 admin.site.register(TicketType)
 admin.site.register(TicketStatus)
 admin.site.register(TicketComment)
 admin.site.register(TicketPriority)
+admin.site.register(TicketAttachment)
