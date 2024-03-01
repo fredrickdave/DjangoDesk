17f9ad5301e47e4d9ee1a93886affd9cd0755112
Fredrick Dave Santulan <(none)>
2024-03-02 00:11:54 +0800

    Change debug mode for render deployment

--- djangodesk/settings.py
+++ djangodesk/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = []
 
