a02013de51c568ec4994d8e42e7a8bdca9f12510
Fredrick Dave Santulan <(none)>
2024-03-07 17:52:22 +0800

    Add assigned agent and ticket author to filter options

--- apps/tickets/filters.py
+++ apps/tickets/filters.py
@@ -1,4 +1,5 @@
 import django_filters
+from django.db.models import Q
 
 from .models import Ticket
 
@@ -10,7 +11,7 @@ class TicketFilter(django_filters.FilterSet):
 
     class Meta:
         model = Ticket
-        fields = {"ticket_number", "summary", "issue_type", "status", "created_at"}
+        fields = {"ticket_number", "summary", "issue_type", "status", "created_at", "created_by", "assigned_agent"}
 
     # https://django-filter.readthedocs.io/en/stable/guide/usage.html#request-based-filtering
     @property
@@ -18,9 +19,10 @@ class TicketFilter(django_filters.FilterSet):
         parent = super().qs
         author = getattr(self.request, "user", None)
 
-        # Override qs method to filter tickets based on user's role. Show user's created tickets if customer.
-        # Show all tickets if support agent or Admin
+        # Override qs method to filter tickets based on user's role.
         if self.request.user.role == 3:
+            # Filter tickets by author if authenticated user is customer
             return parent.filter(created_by=author)
         elif self.request.user.role == 1 or self.request.user.role == 2:
-            return parent
+            #  Filter both all open tickets, and tickets assigned to the agent
+            return parent.filter(Q(assigned_agent=author) | Q(status=1))
