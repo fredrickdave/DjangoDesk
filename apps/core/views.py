7adc43030d7c0b9c50f26f8bbc1ac38e6e4fceb8
Fredrick Dave Santulan <(none)>
2024-03-07 17:56:02 +0800

    Remove unused import

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import HttpResponse, render
+from django.shortcuts import render
 
 from ..tickets.models import Ticket
 
